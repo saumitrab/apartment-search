a7a1e06eb8a1caef917a391ab89a45117ff8b2e7
Saumitra Bhanage <sbhanage@groupon.com>
2018-08-25 01:41:46 -0700

    Add email support

--- lambda.js
+++ lambda.js
@@ -2,9 +2,43 @@
 
 const findApts = require('./src');
 
+const aws = require('aws-sdk');
+const ses = new aws.SES({
+  region: 'us-east-1'
+});
+
+function sendEmail(content) {
+  const eParams = {
+    Destination: {
+      ToAddresses: ["100friends@gmail.com"]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: content.toString()
+        }
+      },
+      Subject: {
+        Data: "New Apts Alert"
+      }
+    },
+    Source: "100friends@gmail.com"
+  };
+
+  console.log('EMAIL: sending');
+  ses.sendEmail(eParams, function (err, data) {
+    if (err) console.log('EMAIL error', err);
+    else {
+      console.log('EMAIL sent');
+    }
+  });
+}
+
 module.exports.run = async (event) => {
+  const apts = await findApts();
+  sendEmail(apts);
   return {
     statusCode: 200,
-    body: JSON.stringify(await findApts()),
+    body: JSON.stringify(apts),
   }
 }
